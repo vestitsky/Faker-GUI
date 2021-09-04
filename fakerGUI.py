5532dc304b2ec1737a49ef326eb1930548d3978e
Alexey <liosha.tkachiov@icloud.com>
2021-09-04 19:03:52 +0300

    Added updatable name with Reboot button

--- fakerGUI.py
+++ fakerGUI.py
@@ -1,14 +1,23 @@
 import faker
 from tkinter import *
 
-root = Tk()
-fake = faker.Faker()
-name = fake.name()
+def tkFaker():
+    def addToClip():
+        root.clipboard_clear()
+        root.clipboard_append(name)
 
+    def update():
+        root.destroy()
+        tkFaker()
 
-Label(root, text=name).pack()
-Button(root, text='Clip', command=lambda:root.clipboard_append(name)).pack()
-Button(root, text='New', command=lambda:name).pack()
+    root = Tk()
+    root.geometry("350x150+220+80")
+    fake = faker.Faker()
+    name = fake.name()
+    Label(root, text=name).grid(row=0,column=0)
+    Button(root, text='Clip', command=addToClip).grid(row=0,column=1)
+    Button(root, text='Reboot', command=update).grid(row=1)
 
+    root.mainloop()
 
-root.mainloop()
+tkFaker()
